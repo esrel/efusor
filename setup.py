c2da0a6a5f5b3f9e2d0e41b8e19972842d891e8c
Evgeny A. Stepanov <esrel@hotmail.com>
2024-01-03 13:02:51 +0100

    cleaning

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+import os
+
+
+def read(path):
+    return open(os.path.join(os.path.dirname(__file__), path)).read()
+
+
+setup(
+    name='efusor',
+    url='https://github.com/esrel/efusor',
+    version='0.1.0',
+    author='Evgeny A. Stepanov',
+    author_email='stepanov.evgeny.a@gmail.com',
+    description='Extended Decision Fusion',
+    readme="README.md",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    package_dir={'': "src"},
+    packages=find_packages('src'),
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+    python_requires='>=3.10'
+)
